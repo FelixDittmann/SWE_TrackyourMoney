63e76ea8cae9b95f57787f30f64a3b9621a6c4c6
JanikDa <janik.dauner@gmail.com>
2024-05-29 11:14:32 +0200

    erste Input Validierung bei Ausgabe anlegen

--- app/src/main/java/com/example/trackyourmoney/ui/addAusgabe/AddAusgabenActivity.java
+++ app/src/main/java/com/example/trackyourmoney/ui/addAusgabe/AddAusgabenActivity.java
@@ -2,8 +2,11 @@ package com.example.trackyourmoney.ui.addAusgabe;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,8 +29,8 @@ public class AddAusgabenActivity extends AppCompatActivity {
     EditText nameInput;
     EditText betragInput;
     EditText dateInput;
-    EditText kategorieIdInput;
-    EditText wiederholendInput;
+    Spinner kategorieIdInput;
+    CheckBox wiederholendInput;
     EditText wiederholungsintervallInput;
     TextView textView;
 
@@ -45,21 +48,43 @@ public class AddAusgabenActivity extends AppCompatActivity {
         nameInput = (EditText) findViewById(R.id.nameInput);
         betragInput = (EditText) findViewById(R.id.betragInput);
         dateInput = (EditText) findViewById(R.id.dateInput);
-        kategorieIdInput = (EditText) findViewById(R.id.kategorieIdInput);
-        wiederholendInput = (EditText) findViewById(R.id.wiederholendInput);
+        kategorieIdInput = (Spinner) findViewById(R.id.kategorieIdInput);
+        wiederholendInput = (CheckBox) findViewById(R.id.wiederholendInput);
         wiederholungsintervallInput = (EditText) findViewById(R.id.wiederholungsintervallInput);
 
         textView = (TextView) findViewById(R.id.testView);
+
+        //TODO Kategorie und Id aus Datenbank auslesen und in Relation abspeichern
+        //String[][] kategorien = ;
+        //ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, kategorien);
+        //kategorieIdInput.setAdapter(adapter);
     }
 
     public void validation(View view) {
         //TODO validation algorithm
+        String valid = "";
         name = nameInput.getText().toString();
-        //betrag = Double.valueOf(betragInput.getText().toString());
-        //date = Long.valueOf(dateInput.getText().toString());
+        try {
+            betrag = Double.valueOf(betragInput.getText().toString());
+        } catch (Exception e) {
+            valid += "Unzulässiger Betrag!\n";
+        }
+        try {
+            date = Long.valueOf(dateInput.getText().toString());
+        } catch (Exception e) {
+            valid += "Unzulässige Datumsschreibweise!\n";
+        }
         //kategorieId = Long.valueOf(kategorieIdInput.getText().toString());
-        //wiederholend = Boolean.valueOf(wiederholendInput.getText().toString());
-        //wiederholungsintervall = Integer.valueOf(wiederholungsintervallInput.getText().toString());
-        textView.setText(name);
+        wiederholend = Boolean.valueOf(wiederholendInput.getText().toString());
+        try {
+            wiederholungsintervall = Integer.valueOf(wiederholungsintervallInput.getText().toString());
+        } catch (Exception e) {
+            valid += "Falsche Angabe des Wiederholungsintervalls!";
+        }
+        textView.setText(valid);
+
+        if (valid == ""){
+            //TODO add Ausgabe
+        }
     }
 }
--- app/src/main/java/com/example/trackyourmoney/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/trackyourmoney/ui/home/HomeFragment.java
@@ -25,10 +25,9 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
